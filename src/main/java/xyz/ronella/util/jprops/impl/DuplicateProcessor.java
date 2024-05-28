bd012134391102dcba3dc8e89a708ebb4f54bd58
Ron Webb <ron@ronella.xyz>
2024-05-29 09:20:45 +1200

    Initial implementation of DuplicateProcessor.

--- src/main/java/xyz/ronella/util/jprops/impl/DuplicateProcessor.java
+++ src/main/java/xyz/ronella/util/jprops/impl/DuplicateProcessor.java
@@ -1,36 +1,26 @@
 package xyz.ronella.util.jprops.impl;
 
-import xyz.ronella.trivial.handy.RegExMatcher;
+import org.slf4j.LoggerFactory;
+import xyz.ronella.logging.LoggerPlus;
+import xyz.ronella.util.jprops.MetaGenerator;
 import xyz.ronella.util.jprops.Processor;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.LinkedHashMap;
-import java.util.Optional;
-import java.util.Scanner;
 
 public class DuplicateProcessor implements Processor {
 
+    private final static LoggerPlus LOG = new LoggerPlus(LoggerFactory.getLogger(DuplicateProcessor.class));
+
     @Override
     public void process(final File props) {
-        try(final var fileReader = new Scanner(props)) {
-            final var pairs = new LinkedHashMap<String, String>();
-            while(fileReader.hasNextLine()) {
-                final var rawLine = fileReader.nextLine();
-                RegExMatcher.find("^(\\s*[a-zA-Z_].*?)=(.*)$", rawLine, ___matcher -> {
-                    final var key = ___matcher.group(1);
-                    final var value = ___matcher.group(2);
-                    final var pair = Optional.of(pairs.get(key));
-
-
-
-                    System.out.println(">>>" + ___matcher.group(1) + "," + ___matcher.group(2));
-                });
+        try(final var gLOG = LOG.groupLog("process")) {
+            final var metaGen = new MetaGenerator(props);
+            final var metadata = metaGen.getMetadata().entrySet().stream()
+                    .filter(___entrySet -> ___entrySet.getValue().count() > 1)
+                    .toList();
+            for (final var meta : metadata) {
+                gLOG.warn("[%s] field duplicated %d times.", meta.getKey(), meta.getValue().count());
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         }
     }
-
-    public static record MetaData(String value, int count) {}
 }
