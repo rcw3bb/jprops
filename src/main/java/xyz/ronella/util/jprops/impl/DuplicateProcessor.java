ba3beb83ab8b85d327a6f63e2ea2dad22111513b
Ron Webb <ron@ronella.xyz>
2024-06-07 11:47:07 +1200

    Make the dedupe output to file.

--- src/main/java/xyz/ronella/util/jprops/impl/DuplicateProcessor.java
+++ src/main/java/xyz/ronella/util/jprops/impl/DuplicateProcessor.java
@@ -7,7 +7,11 @@ import xyz.ronella.util.jprops.JPropsException;
 import xyz.ronella.util.jprops.meta.MetaGenerator;
 import xyz.ronella.util.jprops.meta.PropsMeta;
 import xyz.ronella.util.jprops.util.ArgsMgr;
+import xyz.ronella.util.jprops.util.FileMgr;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 
@@ -34,11 +38,27 @@ public class DuplicateProcessor extends AbstractProcessor {
             final var props = argsMgr.getProps();
             final var metaGen = new MetaGenerator(props);
             try {
+                gLOG.info("Processing %s", props.getAbsolutePath());
 
-                metaGen.getMetadata().forEach((key, value) ->
-                        System.out.printf("%s=%s%s", key, value.currentValue(), value.osType().getEOL().eol()));
+                final var tmpFile = FileMgr.createTmpFile(props);
+                gLOG.info("Temp file created: %s", tmpFile.getAbsolutePath());
 
-            } catch (JPropsException e) {
+                try(final var writer = new PrintWriter(new FileWriter(tmpFile))) {
+                    metaGen.getMetadata().forEach((key, value) -> {
+                        final var isDuplicated = value.count() > 1;
+
+                        if (isDuplicated) {
+                            gLOG.info("[%s] just added once", key);
+                        }
+
+                        writer.printf("%s=%s%s", key, value.currentValue(), value.osType().getEOL().eol());
+                    });
+                }
+
+                FileMgr.safeMove(tmpFile, props).ifPresent(___backupFile ->
+                        gLOG.info("Back file created: %s", ___backupFile));
+
+            } catch (JPropsException | IOException e) {
                 gLOG.error(LOG.getStackTraceAsString(e));
             }
         }
