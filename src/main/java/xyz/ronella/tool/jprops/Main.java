edf8e1d5b136ca732e62646d6b1d41fa642a7021
Ron Webb <ron@ronella.xyz>
2024-06-21 10:10:31 +1200

    The executable now supports configurable log config.

--- src/main/java/xyz/ronella/tool/jprops/Main.java
+++ src/main/java/xyz/ronella/tool/jprops/Main.java
@@ -3,8 +3,10 @@ package xyz.ronella.tool.jprops;
 import xyz.ronella.logging.LoggerPlus;
 import org.slf4j.LoggerFactory;
 import xyz.ronella.tool.jprops.util.AppInfo;
+import xyz.ronella.tool.jprops.util.FileMgr;
 import xyz.ronella.tool.jprops.util.MissingCommandException;
 import xyz.ronella.tool.jprops.util.ArgsMgr;
+import xyz.ronella.trivial.handy.PathFinder;
 
 /**
  * The Main class is the entry point of the application.
@@ -13,6 +15,22 @@ import xyz.ronella.tool.jprops.util.ArgsMgr;
  * @since 1.0.0
  */
 final public class Main {
+
+    static {
+        final var confDir = FileMgr.getConfDir();
+        confDir.ifPresent(___confDir -> {
+            final var logPath = PathFinder.getBuilder("logback.xml")
+                    .addPaths(".", ___confDir.getAbsolutePath())
+                    .build();
+            final var optLogFile = logPath.getFile();
+            if (optLogFile.isPresent()) {
+                final var logFile = optLogFile.get();
+                if (logFile.exists()) {
+                    System.setProperty("logback.configurationFile", logFile.getAbsolutePath());
+                }
+            }
+        });
+    }
 
     private final static LoggerPlus LOGGER_PLUS = new LoggerPlus(LoggerFactory.getLogger(Main.class));
 
